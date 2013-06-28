0f42a9c9d1b261f486e3fb28d747869b2adb454b
admin <borovikv.vladimir@gmail.com>
2013-06-28 17:55:30 +0300

    refactoring QUtils

--- Utils/QUtils.py
+++ Utils/QUtils.py
@@ -6,11 +6,17 @@ Created on Apr 5, 2013
 from PyQt4 import QtGui, QtCore
 from PyWarehouse.Settings import getPathToIcon
 
-def openFile(path):
-    f = QtCore.QFile(path)
-    success = f.open(QtCore.QFile.WriteOnly | QtCore.QFile.Truncate)
-    return success, f
+class Icon(QtGui.QIcon):
+    window = "Icon"
+    arrowLeft = "arrowLeft"
+    arrowRight = "arrowRight"
+    preferences = "preferences-icon"
+    edit = "Edit"
+    openFolder = "Show"
     
+    def __init__(self, name):
+        super(Icon, self).__init__(getPathToIcon(name))
+
 def makeButton(name, icon=None, func=None):
     if icon:
         but = QtGui.QPushButton(icon, name)
@@ -33,10 +39,8 @@ def getGoodUrl(link):
     
     # check if it looks like a qualified URL. try parsing it end see
     hasSchema = test.exactMatch(urlStr)
-    if hasSchema:
-        url = QtCore.QUrl(urlStr, QtCore.QUrl.TolerantMode)
-        if url.isValid():
-            return url
+    if hasSchema and QtCore.QUrl(urlStr, QtCore.QUrl.TolerantMode).isValid():
+        return QtCore.QUrl(urlStr, QtCore.QUrl.TolerantMode)
     
     # Might be a file
     if QtCore.QFile.exists(urlStr):
@@ -69,20 +73,10 @@ def getShortcut(*keys):
 
 def addShortcutAction(obj, shortcut, func):
     act = QtGui.QAction(obj)
-    act.setShortcut(shortcut)
     act.triggered.connect(func)
+    act.setShortcut(shortcut)
     obj.addAction(act)
 
-class Icon(QtGui.QIcon):
-    window = "Icon"
-    arrowLeft = "arrowLeft"
-    arrowRight = "arrowRight"
-    preferences = "preferences-icon"
-    edit = "Edit"
-    openFolder = "Show"
-    
-    def __init__(self, name):
-        super(Icon, self).__init__(getPathToIcon(name))
 
 def createQAction(obj, name, func):
     addAction = QtGui.QAction(name, obj)
