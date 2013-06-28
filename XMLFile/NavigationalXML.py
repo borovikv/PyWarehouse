880842a955568d0ad9be037a8aa6ed457671aa1f
admin <borovikv.vladimir@gmail.com>
2013-06-28 16:45:09 +0300

    refactoring xmlNav

--- XMLFile/NavigationalXML.py
+++ XMLFile/NavigationalXML.py
@@ -11,52 +11,66 @@ class NavigationalXML(XML):
     def __init__(self, xml_file, xml_str=None, parseFromFile=False):
         XML.__init__(self, xml_file, xml_str, parseFromFile)
         root = self.getDocumentElement()
-        self.elements = {XmlNames.rootName: root}
+        self.elements = { XmlNames.rootName: root }
         self.initElements(root)
         
+
     def initElements(self, root):
         if root.hasChildNodes():
             nodeList = root.childNodes
             for node in nodeList:
-                self.elements[node.getAttribute(XmlNames.attributeName)] = node
+                self.elements[self.getName(node)] = node
                 self.initElements(node)
+
+    def getName(self, node):
+        return node.getAttribute(XmlNames.attributeName)
+
     
-    def createElement(self, parent_element, new_element):
-        parent_element = unicode(parent_element)
-        new_element = unicode(new_element)
-        if parent_element:
-            parent = self.elements[parent_element]
-        else:
-            parent = self.getDocumentElement()
-            
-        new = XML.createElement(self, XmlNames.tagName)
-        new.setAttribute(XmlNames.attributeName, new_element)
-        parent.appendChild(new)
+
+    def getCurrentElement(self, name):
+        if name and self.elements.has_key(name):
+            return self.elements[name]
+        return self.getDocumentElement()
+
+    def createElement(self, name, newName):
+        name = unicode(name)
+        newName = unicode(newName)
+
+        newElem = XML.createElement(self, XmlNames.tagName)
+        newElem.setAttribute(XmlNames.attributeName, newName)
+        parent = self.getCurrentElement(name)
+        parent.appendChild(newElem)
         self.save()
-        self.elements[new_element] = new
-        
-    def renameElement(self, old, new):
-        old = unicode(old)
-        new = unicode(new)
-        element = self.elements.pop(old)
-        element.removeAttribute(XmlNames.attributeName)
-        element.setAttribute(XmlNames.attributeName, new)
-        self.elements[new] = element
-    
-    def deleteElement(self, element):
-        element = unicode(element)
-        element = self.elements.pop(element)
-        parent = element.parentNode
-        parent.removeChild(element)
-        self.save()
+        self.elements[newName] = newElem
         
     
-    def move(self, parent, newChild, refChild):
-        parent = self.elements[unicode(parent)]
-        newChild = self.elements[unicode(newChild)]
+    def renameElement(self, oldName, newName):
+        oldName = unicode(oldName)
+        newName = unicode(newName)
+        if self.elements.has_key(oldName):
+            element = self.elements.pop(oldName)
+            element.removeAttribute(XmlNames.attributeName)
+            element.setAttribute(XmlNames.attributeName, newName)
+            self.elements[newName] = element
+    
+    
+    def deleteElement(self, name):
+        name = unicode(name)
+        if self.elements.has_key(name):
+            element = self.elements.pop(name)
+            parent = element.parentNode
+            parent.removeChild(element)
+            self.save()
+        
+    
+    def move(self, parent, newChild, refChild=None):
+        parent = self.elements.get(unicode(parent))
+        newChild = self.elements.get(unicode(newChild))
         if refChild:
-            refChild = self.elements[unicode(refChild)]
-        parent.insertBefore(newChild, refChild)
+            refChild = self.elements.get(unicode(refChild))
+        if parent and newChild:    
+            parent.insertBefore(newChild, refChild)
+    
     
     def getId(self):
         i = self.elements[XmlNames.rootName].getAttribute("id")
