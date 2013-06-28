5f9ac2495e77768751d3b23b8538aa99d31cf55c
admin <borovikv.vladimir@gmail.com>
2013-06-28 11:45:41 +0300

    add Settings.py

---
+++ MainFrame/Settings.py
@@ -0,0 +1,16 @@
+'''
+Created on Jun 28, 2013
+
+@author: drifter
+'''
+import os
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+ICONS = os.path.join(PROJECT_PATH, 'images')
+USER_SETTINGS = os.path.join(PROJECT_PATH, "Settings")
+
+
+        
+
+def getPathToIcon(name):
+    return os.path.join(ICONS, name + '.png')
