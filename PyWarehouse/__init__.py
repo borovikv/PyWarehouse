e0c6c744a2fa0d584e8f4302cc2f0698b0d41465
admin <borovikv.vladimir@gmail.com>
2013-07-02 12:16:47 +0300

    ref

--- PyWarehouse/__init__.py
+++ PyWarehouse/__init__.py
@@ -1,10 +1,1 @@
-       
 
-if __name__ == "__main__":
-    import sys   
-    from MainFrame.Warehouse import Warehouse
-    from PyQt4 import QtGui
-    
-    app = QtGui.QApplication(sys.argv)
-    warehouse = Warehouse()
-    sys.exit(app.exec_())
